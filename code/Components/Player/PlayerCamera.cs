7990a495270946b9823f604ae1c6219f39be83ec
walker407 <hscott407@gmail.com>
2024-06-07 09:01:18 -0500

    adding initial code for player camera / inventory components

---
+++ code/Components/Player/PlayerCamera.cs
@@ -0,0 +1,20 @@
+﻿namespace ItemBuilder;
+
+public partial class PlayerCamera : Component
+{
+	[RequireComponent] public CameraComponent Camera { get; set; }
+
+	private PlayerController PlayerController { get; set; }
+
+	protected override void OnUpdate()
+	{
+		base.OnUpdate();
+
+		if(PlayerController.IsValid())
+		{
+			PlayerController = Player.Local.Controller;
+
+			return;
+		}
+	}
+}
---
+++ code/Components/Player/PlayerInventory.cs
@@ -0,0 +1,6 @@
+﻿namespace ItemBuilder;
+
+public partial class PlayerInventory : Component
+{
+	[RequireComponent] public Player Player { get; set; }
+}
