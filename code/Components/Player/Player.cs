ee5f3599971f899d7b024749aacb6ea8cf51ef54
walker407 <hscott407@gmail.com>
2024-06-07 08:55:57 -0500

    Adding initial code for player / player controller components

---
+++ code/Components/Player/Player.cs
@@ -0,0 +1,21 @@
+﻿namespace ItemBuilder;
+
+public partial class Player : Component
+{
+	[RequireComponent] public PlayerController Controller { get; set; }
+
+	private static Player _Local = null;
+
+	public static Player Local
+	{
+		get
+		{
+			if ( !_Local.IsValid() )
+			{
+				_Local = Game.ActiveScene.GetAllComponents<Player>().FirstOrDefault( x => x.Network.IsOwner );
+			}
+
+			return _Local;
+		}
+	}
+}
---
+++ code/Components/Player/PlayerController.cs
@@ -0,0 +1,10 @@
+﻿using Sandbox.Citizen;
+
+namespace ItemBuilder;
+
+public class PlayerController : Component
+{
+	[RequireComponent] public CharacterController CharacterController { get; set; }
+	[RequireComponent] public CitizenAnimationHelper AnimationHelper { get; set; }
+
+}
