b67035dc676b3eec5ee6f5e6542c34c5c53cbf9f
walker407 <hscott407@gmail.com>
2024-06-07 08:44:37 -0500

    Cleaning up / addingi initial code for item generator tool

---
+++ Editor/ItemGenerator.cs
@@ -0,0 +1,8 @@
+﻿using Editor;
+using Sandbox;
+
+public class ItemBuilder : EditorTool<ModelRenderer>
+{
+	public StringProperty ItemName {  get; set; }
+	public StringProperty ItemType { get; set; }
+}
