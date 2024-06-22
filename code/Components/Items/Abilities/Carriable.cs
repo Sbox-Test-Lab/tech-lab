50b5569d6c2f7b0ab2de7f3350a7d57389eb2363
walker407 <hscott407@gmail.com>
2024-06-22 14:11:37 -0500

    Adding initial code for Carriable item ability

--- code/Components/Items/Abilities/Carriable.cs
+++ code/Components/Items/Abilities/Carriable.cs
@@ -2,5 +2,24 @@
 
 public class Carriable : BaseItemAbility
 {
+	public delegate void PickupDelegate( GameObject user, Item item );
+	public static PickupDelegate OnItemPickup { get; set; }
 
+	[RequireComponent] public Interactable Interaction { get; set; }
+	[RequireComponent] public Item Item { get; set; }
+
+	protected override void OnStart()
+	{
+		base.OnStart();
+
+		Interaction.OnInteraction += OnInteraction;
+	}
+
+	public void OnInteraction(GameObject user, GameObject gameObject)
+	{
+		if ( gameObject != GameObject )
+			return;
+
+		OnItemPickup?.Invoke( user, Item );
+	}
 }
